179c3383bdb0b8ed4c372b6e095d5328d764c1bd
Marek <mareksax@wp.pl>
2025-02-06 10:30:06 +0100

    feat: add catchError utility function

---
+++ utils/catch-error.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from 'express';
+
+type MiddlewareFunction = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => Promise<void> | void;
+
+const catchError = (middleware: MiddlewareFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await middleware(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default catchError;
