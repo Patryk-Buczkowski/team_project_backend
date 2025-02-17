41c2579ab23d3696cad3b35890c9471e452440fd
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-02-17 17:51:43 +0100

    fix: standardize string quotes in server.ts and comment out database connection logic

--- server.ts
+++ server.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
-import dotenv from 'dotenv';
-import app from './app';
-import sequelize from 'utils/db';
+import dotenv from "dotenv";
+import app from "./app";
+import sequelize from "utils/db";
 
 dotenv.config();
 
@@ -9,13 +9,13 @@ const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    await sequelize.authenticate();
-    console.log('Database connection successful');
+    // await sequelize.authenticate(); // baza musiałaby byc na serwerze zewnetrznym
+    console.log("Database connection successful");
 
-    await sequelize.sync();
-    console.log('Database synchronized');
-    console.log('Database connection successful');
-    console.log('Indexes created for User model');
+    // await sequelize.sync();
+    // console.log("Database synchronized");
+    // console.log("Database connection successful");
+    // console.log("Indexes created for User model");
     app.listen(PORT, () => {
       console.log(`
         Server running port: ${PORT}
@@ -25,7 +25,7 @@ const startServer = async () => {
     if (err instanceof Error) {
       console.error(`Server not running. Error message: ${err.message}`);
     } else {
-      console.error('Server not running. Unknown error.');
+      console.error("Server not running. Unknown error.");
     }
     process.exit(1);
   }
