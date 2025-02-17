437f3b62f57a59dc81b9077073dfc76a1cb8ef95
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-02-17 17:55:17 +0100

    fix: comment out error middleware in app.ts and error.middleware.ts

--- middlewares/error.middleware.ts
+++ middlewares/error.middleware.ts
@@ -1,30 +1,29 @@
-/* eslint-disable no-console */
-import { ErrorRequestHandler, Request, Response } from "express";
-import { ApiError } from "../exceptions/api.error";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
+// import { ErrorRequestHandler, Request, Response } from "express";
+// import { ApiError } from "../exceptions/api.error";
+// import { ReasonPhrases, StatusCodes } from "http-status-codes";
 
-const errorMiddleware: ErrorRequestHandler = (
-  error: unknown,
-  req: Request,
-  res: Response,
-) => {
-  if (error instanceof ApiError) {
-    const { status, message, errors } = error;
+// const errorMiddleware: ErrorRequestHandler = (
+//   error: unknown,
+//   req: Request,
+//   res: Response,
+// ) => {
+//   if (error instanceof ApiError) {
+//     const { status, message, errors } = error;
 
-    console.error("Handled API error:", error.message, error.errors);
+//     console.error("Handled API error:", error.message, error.errors);
 
-    res.status(status).send({ message, errors });
-    return;
-  }
+//     res.status(status).send({ message, errors });
+//     return;
+//   }
 
-  console.error("Unhandled error:", error);
+//   console.error("Unhandled error:", error);
 
-  res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
-    message:
-      error instanceof Error
-        ? error.message
-        : ReasonPhrases.INTERNAL_SERVER_ERROR,
-  });
-};
+//   res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+//     message:
+//       error instanceof Error
+//         ? error.message
+//         : ReasonPhrases.INTERNAL_SERVER_ERROR,
+//   });
+// };
 
-export default errorMiddleware;
+// export default errorMiddleware;
