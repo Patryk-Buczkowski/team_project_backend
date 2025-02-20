effc4a2d49e004f35b66598aab7333dc3cf9e691
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-02-20 20:31:29 +0100

    test

--- routes/auth.router.ts
+++ routes/auth.router.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import catchError from "../utils/catch-error";
 import { authController } from "../controllers/auth.controller";
 
@@ -8,5 +8,8 @@ authRouter.post("/register", catchError(authController.registerUser));
 authRouter.post("/login", catchError(authController.loginUser));
 authRouter.post("/logout", catchError(authController.logoutUser));
 authRouter.get("/refresh", catchError(authController.refreshUser));
+authRouter.get("/dupa", async (req: Request, res: Response) => {
+  res.json("duek");
+});
 
 export default authRouter;
