6c5e34061a6cd1b39c10b2a30460e89466177a95
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-03-22 07:06:23 +0100

    fix: refactor Sequelize initialization for improved readability and configuration

--- utils/db.ts
+++ utils/db.ts
@@ -1,23 +1,22 @@
-// import dotenv from "dotenv";
+import dotenv from "dotenv";
 import { Sequelize } from "sequelize";
-// dotenv.config();
+dotenv.config();
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  logging: false,
-  // locally commented dialectOptions
-
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  process.env.DB_NAME as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-    application_name: "team_project_backend",
   },
-});
+);
 
 export default sequelize;
